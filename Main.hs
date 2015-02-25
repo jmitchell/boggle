36db448c3bb949a57fd6d49a985f59743b474982
Jake Mitchell <jacob.d.mitchell@gmail.com>
2015-02-24 18:58:36 -0800

    add sample three letter word dictionary and demo in Main.hs (see issue #2)

--- Main.hs
+++ Main.hs
@@ -3,6 +3,9 @@ module Main where
 import Boggle
 
 import Control.Monad
+import Data.List
 
 main :: IO ()
-main = liftM (trayString '_') (shuffledTray (5,5)) >>= putStrLn
+main = do
+  (shuffledTray (4,4)) >>= \tray ->
+    putStrLn $ intercalate "\n" $ [trayString '_' tray, ""] ++ wordsOnTray tray threeLetterWords
